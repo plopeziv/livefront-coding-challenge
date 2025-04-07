0a2bbe177f275b8c9bda0d5b63741d3224ff85e5
plopez9 <plopez@8thlight.com>
2025-04-07 05:25:16 -0500

    Add dynamic routing to team pages

---
+++ src/app/scoring_leaders/[team_name]/components/ScoringTable.tsx
@@ -0,0 +1,43 @@
+export default function StandingsTable(props) {
+  const headers = props.headers;
+  const rowData = props.rowData;
+
+  return (
+    <table className='text-center'>
+      <thead className='bg-[#2b2d42]'>
+        <tr>
+          {headers.map((header) => (
+            // eslint-disable-next-line react/jsx-key
+            <th key={header.field} className='mx-3 min-w-[90px]'>
+              {header.field}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody className='bg-[rgba(141,153,174,0.88)]'>
+        {rowData.map((item, index) => (
+          <tr
+            key={index}
+            onClick={() => handleClick(item.team.name)}
+            className={`${
+              index % 2 === 0
+                ? "bg-[rgba(141,153,174,0.88)]"
+                : "bg-[rgba(224, 232, 235, 0.88)]"
+            } hover:bg-[rgba(180,200,220,0.88)] cursor-pointer`}
+          >
+            <td>{item.position}</td>
+            <td>{item.team.name}</td>
+            <td>{item.playedGames}</td>
+            <td>{item.won}</td>
+            <td>{item.draw}</td>
+            <td>{item.lost}</td>
+            <td>{item.goalsFor}</td>
+            <td>{item.goalsAgainst}</td>
+            <td>{item.goalDifference}</td>
+            <td>{item.points}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
--- src/app/scoring_leaders/[team_name]/page.tsx
+++ src/app/scoring_leaders/[team_name]/page.tsx
@@ -1,9 +1,36 @@
 "use client";
 
 import { useParams } from "next/navigation";
+import ScoringTable from "./components/ScoringTable";
 
 export default function ScoringLeaders() {
   const { team_name } = useParams();
+
+  const colNames = [
+    { field: "Position" },
+    { field: "Name" },
+    { field: "Played Games" },
+    { field: "Won" },
+    { field: "Draw" },
+    { field: "Lost" },
+    { field: "Goals For" },
+    { field: "Goals Against" },
+    { field: "+ -" },
+    { field: "Points" },
+  ];
+
+  const blankRows = Array.from({ length: 10 }, (_, i) => ({
+    Position: i + 1,
+    team: { name: "" },
+    "Played Games": "",
+    Won: "",
+    Draw: "",
+    Lost: "",
+    "Goals For": "",
+    "Goals Against": "",
+    "+ -": "",
+    Points: "",
+  }));
 
   const deslugify = (slug: string | string[]): string => {
     let singleSlug;
@@ -21,7 +48,6 @@ export default function ScoringLeaders() {
 
   const desluggedTeam = deslugify(team_name);
 
-  console.log(deslugify(team_name));
   return (
     <div className='text-5xl mb-4 drop-shadow-[0_2px_4px_rgba(0,0,0,0.7)] text-center'>
       <h1>{desluggedTeam}</h1>
